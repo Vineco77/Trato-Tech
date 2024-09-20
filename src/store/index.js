ec28f5ab0befe7a2f36c2e2da8e8b340106d65c3
Vinicius Ribeiro <vinii.viniciusribeiro@gmail.com>
2024-09-20 17:51:30 -0300

    Refactoring project to Redux Saga!

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,12 @@ import categoriesSlice from "./reducers/categories";
 import itemsSlice from "./reducers/items";
 import cartSlice from "./reducers/cart";
 import searchSlices from "./reducers/search";
-import { categoriesListener } from "./middlewares/categories";
+// import { categoriesListener } from "./middlewares/categories";
 import { itemsListenar } from "./middlewares/items";
+import createSagaMiddleware from "redux-saga";
+import { categoriesSaga } from "./sagas/categories";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
@@ -15,9 +19,12 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().prepend(
-      categoriesListener.middleware,
-      itemsListenar.middleware
+      // categoriesListener.middleware,
+      itemsListenar.middleware,
+      sagaMiddleware
     ),
 });
+
+sagaMiddleware.run(categoriesSaga);
 
 export default store;

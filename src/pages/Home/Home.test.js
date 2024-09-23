2dfb5519b2cf7bca9c5a9abcd9fdf8329200d599
Vinicius Ribeiro <vinii.viniciusribeiro@gmail.com>
2024-09-23 15:58:03 -0300

    Testing Library Config Done

---
+++ src/pages/Home/Home.test.js
@@ -0,0 +1,9 @@
+import { render } from "test-utils";
+import Home from ".";
+
+describe("Testando página Home", () => {
+  test("testando", () => {
+    render(<Home />);
+    expect(true).toBeTruthy();
+  });
+});
